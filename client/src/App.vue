2f695c14f68f70a34a5c7f47030a7263b5df7a7d
OnSebii <lang.sebastian.006@gmail.com>
2021-09-18 17:39:17 +0200

    Frage 1

---
+++ client/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-bottom-navigation class="cyan" app>
+      <span class="white--text title">v-bottom-navigation</span>
+    </v-bottom-navigation>
+    <v-navigation-drawer class="green" app>
+      <span class="white--text title">v-navigation-drawer</span>
+    </v-navigation-drawer>
+
+    <v-app-bar class="yellow" app>
+      <span class="title">v-app-bar</span>
+    </v-app-bar>
+    <v-main class="blue">
+      <span class="white--text title">v-main</span>
+    </v-main>
+    <v-footer class="purple" app>
+      <span class="white--text title mx-auto">v-footer</span>
+    </v-footer>
+        <v-system-bar class="pink" app>
+      <span class="white--text title">v-system-bar</span>
+    </v-system-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
