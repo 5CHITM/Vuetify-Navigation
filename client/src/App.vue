daeda45e4719046e26a39e84e6cb0f4e31a8650f
OnSebii <lang.sebastian.006@gmail.com>
2021-09-22 14:03:27 +0200

    Aufgabe 5 Pro

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,35 +5,37 @@
       <span class="text-truncate text-h4 blue-grey--text darken-1" style="font-family: Poppins">Fancy Fanta</span>
     </v-app-bar>
     <v-main>
-      <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent absolute>
-        <v-list-item class="px-2">
-          <v-list-item-avatar>
-            <v-img
-              src="https://lh3.googleusercontent.com/fife/AAWUweX2NcfcMet5KvXIuUqgbdbrvhY-vvq4GQ5EMTtCFioQ_GAFLzR84HCGawetWs7J81wHA9Ifh3nsSlnPezjbiLs0UblvF-1IsX7RhcxdjX5woyJ4V7zicSL2i6Smiwx7Ce7Ob2eMm-I1Yvlq8NT6Xoo0WwKa_iHvXB7akDWztc4CtTzmjOEI3R2Gz-IKxWvE_PeHZqNy-dYrDREbFk_uFQPN9Ws1AyCS1ixMTl0gBbOPA7RJyq6MWwqb27LHh1GeGG4LLUaiRJOr_qeS0SXuSaMIf5APZUu35Z0pYWbDE2fkUAtNJqh8Vc985rEk-A3X5kxxjqAPH6zuzUWzNs5LgZHm1LIrMgE4pYbwsFxk2wkmnPIk2kS-HSbVAwkmr9Re1kN_aUzF2TlE6A9WHWAbj48V9AO52d-saeoz8IZIH0Du6HbxSgNZkZp5qinrS3GOn6gKrX3JCQ8y1lXljfaKS41UcQRXmTaqIp8l4ItjYmUWMZKTPn24ny8iYB4sVYQipsrNgTkv2ipjgaB4T8fzpYP4vOnD0unf9ks5c5oaWKzCLS-z-FIgs-SRgbkaeqa3kDtM4JO8U5NJDEtY63lsDHuefBfLtwg-o4P96mQQeD7mQK2R5mDTYVDwl2cQFane1hX9-M9ldp4czEm7bXsVQdpzhiRUGnelwlD1Y73VS4o1MSDVKUfgGhXM6BScnD8AXJ6xfNxVEQhKimMyk4SZ3WnNkmA9DlO_2JATMJ1QkOh5Wg=s64-c"
-            ></v-img>
-          </v-list-item-avatar>
+      <div>
+        <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent absolute style="margin-top: 150px; margin-left: 20px; height: 299px; border: 1px solid grey">
+          <v-list-item class="px-2">
+            <v-list-item-avatar>
+              <v-img
+                src="https://lh3.googleusercontent.com/fife/AAWUweWEGpWzxrKHivZtKRlkRSb7fKTUCj9oJUdS72UnNk7w4qDcfzfUmQTIR3nipdon_3mFcJdJZkYyOOJfwe4FRhNOz-YQcY8_ev0WsbcIfuUVFtA5OClXuaVzbZSorFtgH3mCvVY6jgxcCMEBRk3Y-bGK6tptwhr8QQlpRvPSFubteXzSJ1k7inXabNjGA1_9m0PFvV3DLAqK6Bt9RfspoZtvhGdIy9wvJp9USZvavNxj191v4VRsqZ27k2xRNt_OU_xIWOjsCGSxGy8OuzrW60A-9pJ3y5-kBQxB4B08or-W0yB53DvJRy-RZrldSbGH_bQ6ZmSPK54nvHNi0N-XcD3B3gRU-GTIL3-EbjmPA1Uy9yPJUyXWr2mOlvIesM2uL2pIlMfMdl02v-ZtIyt-NRxTQJRMhJkMbSS-9tz7OA37zWyq5tqfzOXaqknbwDUqiDBhii__CZqT_CZ7XbU2WaYDRQZNqBNgTn0vIoBIswW1ZAgj740y3lehQt9PetdPDuEqwcMdL_sLAadmDqI5b-zVJzn_jWZXh1kfNBIrNvRo0FNBBYzuS6b7AkWdz6aKAMH14K3TP0fL3RAYl92epJ9XTz6FmLih65nmY0SprIW0Z4zmNM7HXc1E_GjTzD3KibWc9ghsvKW4RecYrk0y4okCrXO996ZM7sw1FHYjErqpW1MRf8hqCxx7eJOflKNOcF34FCSnk153Fzdcm4avndhxkHyCcAOjpIBiUvJUkd1cHtA=s128"
+              ></v-img>
+            </v-list-item-avatar>
 
-          <v-list-item-title>His Student</v-list-item-title>
+            <v-list-item-title>His Student</v-list-item-title>
 
-          <v-btn icon @click.stop="mini = !mini">
-            <v-icon>mdi-chevron-left</v-icon>
-          </v-btn>
-        </v-list-item>
-
-        <v-divider></v-divider>
-
-        <v-list>
-          <v-list-item v-for="route in routes" :key="route.name" link>
-            <v-list-item-icon>
-              <v-icon>{{ route.icon }}</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-content>
-              <v-list-item-title>{{ route.name }}</v-list-item-title>
-            </v-list-item-content>
+            <v-btn class="grey" icon @click.stop="mini = !mini">
+              <v-icon>mdi-chevron-left</v-icon>
+            </v-btn>
           </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+
+          <v-divider></v-divider>
+
+          <v-list>
+            <v-list-item v-for="route in routes" :key="route.name" :to="route.link" link active-class="orange">
+              <v-list-item-icon>
+                <v-icon>{{ route.icon }}</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ route.name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </div>
       <div class="d-flex">
         <div style="width:250px"></div>
 
